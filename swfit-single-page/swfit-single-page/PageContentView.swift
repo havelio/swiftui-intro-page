88cd231a2006fbb3b5a37ec338fbd490d4390f72
Havelio Henar <havelio.henar@finaccel.co>
2021-03-25 13:33:51 +0700

    add dummy asset

--- swfit-single-page/swfit-single-page/PageContentView.swift
+++ swfit-single-page/swfit-single-page/PageContentView.swift
@@ -13,14 +13,13 @@ struct PageContentView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            RoundedRectangle(cornerRadius: 25)
-                .fill(Color.blue.opacity(0.8))
+            Image("dummy")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
                 .frame(maxHeight: 400)
-                .shadow(color: .black, radius: 4, x: 0, y: 0)
 
-            Text("Lorem Title")
-                .font(.title)
-                .fontWeight(.bold)
+            Text("Ut enim ad minim")
+                .font(.largeTitle)
                 .padding(.top, 20)
 
             Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
